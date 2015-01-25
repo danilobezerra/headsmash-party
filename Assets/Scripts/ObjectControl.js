fa47886af02b1f6e8b952261a1c4bd5f2928a59e
unknown <danilobezerra@outlook.com>
2015-01-25 01:10:18 -0200

    Animation updates

--- Assets/Scripts/ObjectControl.js
+++ Assets/Scripts/ObjectControl.js
@@ -6,8 +6,12 @@ public var dirRight : boolean = false;
 
 function FixedUpdate() {
 	var dir : Vector3 = Vector3(1, 0, 0);
-	if (!dirRight){
+	if (!dirRight) {
 		dir.x = -1;
 	}
 	transform.Translate(dir * speed * Time.deltaTime);
+}
+
+function OnBecameInvisible() {
+	Destroy(gameObject);
 }
