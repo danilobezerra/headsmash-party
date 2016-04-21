30fc25760a264abbc6aecb0de2a95e84f82440fb
Danilo Bezerra <danilobezerra@outlook.com.br>
2016-04-21 18:12:09 -0300

    Credits loading fix

--- Assets/Scripts/IntroController.cs
+++ Assets/Scripts/IntroController.cs
@@ -3,23 +3,38 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class IntroController : MonoBehaviour {
-	[SerializeField] private string sceneName = "MainMenu";
-	[SerializeField] private int waitSeconds = 17;
+	[SerializeField] private string nextSceneName;
+	[SerializeField] private string parameterName;
+	[SerializeField] private int waitSeconds;
 	
 	private Animator animator;
+	private IEnumerator nowLoading;
 	
 	private void Awake() {
 		animator = this.GetComponent<Animator>();
 	}
 	
-	IEnumerator Start() {
-		animator.SetBool("ShowCredits", true);
+	private void Start() {
+		nowLoading = ShowCredits();
+		StartCoroutine(nowLoading);
+	}
+	
+	private void Update() {
+		if (animator.GetBool(parameterName)) {
+			if (Input.anyKeyDown) {
+				StopCoroutine(nowLoading);
+				animator.SetBool(parameterName, false);
+			}
+		}
+	}
+	
+	private IEnumerator ShowCredits() {
 		yield return new WaitForSeconds(waitSeconds);
-		animator.SetBool("ShowCredits", false);
+		animator.SetBool(parameterName, false);
 	}
 	
 	public void LoadLevel() {
 		Debug.Log("Load next scene...");
-		SceneManager.LoadScene(sceneName);
+		SceneManager.LoadScene(nextSceneName);
 	}
 }
