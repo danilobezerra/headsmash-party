b167bf0c2e4e80c7ec4917c6f57d01bbcfbaa892
Danilo Bezerra <danilobezerra@outlook.com.br>
2016-04-21 17:24:53 -0300

    Credits animation update

---
+++ Assets/Scripts/IntroController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class IntroController : MonoBehaviour {
+	[SerializeField] private string sceneName = "MainMenu";
+	[SerializeField] private int waitSeconds = 17;
+	
+	private Animator animator;
+	
+	private void Awake() {
+		animator = this.GetComponent<Animator>();
+	}
+	
+	IEnumerator Start() {
+		animator.SetBool("ShowCredits", true);
+		yield return new WaitForSeconds(waitSeconds);
+		animator.SetBool("ShowCredits", false);
+	}
+	
+	public void LoadLevel() {
+		Debug.Log("Load next scene...");
+		SceneManager.LoadScene(sceneName);
+	}
+}
--- Assets/Scripts/_intro.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class _intro : MonoBehaviour {
-	IEnumerator Start() {
-		yield return new WaitForSeconds(17);
-		Application.LoadLevel("MainMenu");
-	}
-}
