9e61c90e559e72f4012bf4a9f52619780a9ebaa0
Danilo Bezerra <danilobezerra@outlook.com.br>
2016-04-21 22:55:48 -0300

    Decision screen update

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraShake : MonoBehaviour {
+	[RangeAttribute(0, .1f)] [SerializeField] private float yDisplacement;
+
+	private void FixedUpdate() {
+		Vector3 cameraPosition = transform.position;
+		
+		if (cameraPosition.y == 0) {
+			cameraPosition.y += yDisplacement;
+			transform.position = cameraPosition;
+		} else {
+			cameraPosition.y -= yDisplacement;
+			transform.position = cameraPosition;
+		}
+	}
+}
---
+++ Assets/Scripts/FadeOut.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOut : MonoBehaviour {
+	[SerializeField] private Image whiteScreen;
+	
+	private void Update() {
+		if (whiteScreen.color.a > 0) {
+			Color color = whiteScreen.color;
+			color.a -= .01f;
+			whiteScreen.color = color;
+		}
+	}
+}
